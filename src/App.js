e159cf27e9319a51d0714a4c12129c4b38fee1bc
fatemeh <fatemeh.ka84@gmail.com>
2021-04-09 18:04:29 -0400

    Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 function App() {
   return (
+<>
+  <Particles
+  className="particles-canvas"
+    params={{
+     particles:{
+        number:{
 
+          value:20,
+          density:{
+            enabel:true,
+            value_area:900
+          }
+        },
+         shape:{
+
+          type:"star",
+          stroke:{
+            width:1,
+             color:"#fefaf4"
+          }
+         }
+
+
+      }
+
+
+    }}
+  
+  />
   <Navbar />
-
+  <Header />
+</>
   );
 }
 
